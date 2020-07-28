5c05329353ecc21126477df176d46dda3f7fd690
MAXIM <victor-kovalov@yandex.ru>
2020-07-28 17:08:09 +0300

    Change git_client.py

--- git_client/client.py
+++ git_client/client.py
@@ -1,41 +1,40 @@
-import pygit2
-import logging
+import time
+import asyncio
+import aiohttp
 
 from core.config import config
 
-logging.getLogger('malamute_global_logger')
+
+async def fetch(target, file_name, chunk_size=10):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(target) as resp:
+            with open(file_name, 'wb') as fd:
+                while True:
+                    chunk = await resp.content.read(chunk_size)
+                    if not chunk:
+                        break
+                    fd.write(chunk)
 
 
-class CustomRemoteCallbacks(pygit2.RemoteCallbacks):
-    """
-    Сторонняя библиотека pygit2, порт libgit2 c языка си.
-    Позволяет работать с git.
-    """
-    def credentials(self, url, username_from_url, allowed_types):
-        if allowed_types & pygit2.credentials.GIT_CREDENTIAL_USERNAME:
-            return pygit2.Username(config.read_from_conf()['user_name'])
-        elif allowed_types & pygit2.credentials.GIT_CREDENTIAL_SSH_KEY:
-            return pygit2.Keypair("git", "id_rsa.pub", "id_rsa", "")
-        else:
-            return None
+async def main(conf):
+    tasks = []
+    targets = conf['targets']
+    build_dir = conf['build_dir']
+    file_ext = conf['file_extension']
+    chunk_size_bytes = conf['chunk_size']
+    if not targets or not build_dir or not file_ext:
+        raise AssertionError('Field "targets" or "build_dir or file_ext" in malamute.json config is empty. Set it.')
+    for url in targets:
+        file_name = url.split('/')[-3]
+        path_to_build_dir = build_dir + file_name + file_ext
+        task = asyncio.create_task(fetch(url, path_to_build_dir, chunk_size_bytes))
+        tasks.append(task)
+    await asyncio.gather(*tasks)
 
 
-class FetchDataFromGit:
-    @staticmethod
-    def clone_https(repo_name, build_dir, callback):
-        try:
-            pygit2.clone_repository(repo_name, build_dir, callbacks=callback)
-        except Exception as e:
-            logging.debug(e)
-
-    @staticmethod
-    def clone_ssh(self, repo_name, build_dir):
-        try:
-            key_pair = pygit2.Keypair("git", "id_rsa.pub", "id_rsa", "")
-            callbacks = pygit2.RemoteCallbacks(credentials=key_pair)
-            pygit2.clone_repository(repo_name, build_dir, callbacks=callbacks)
-        except Exception as e:
-            logging.debug(e)
-
-
-git_obj = FetchDataFromGit()
+if __name__ == '__main__':
+    conf_obj = config.read_from_conf()
+    start = time.time()
+    asyncio.run(main(conf_obj), debug=True)
+    end = time.time()
+    print(f'Total time : {end - start:.2}')
