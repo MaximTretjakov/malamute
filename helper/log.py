1bb42605b81ef1f27da617c6879c3c1d3f05bed8
MAXIM <victor-kovalov@yandex.ru>
2020-08-13 10:38:19 +0300

    Add logging

--- helper/log.py
+++ helper/log.py
@@ -2,6 +2,7 @@ import logging
 
 
 def setup_custom_logger(name):
+    logging.basicConfig(filename='malamute.log', level=logging.DEBUG)
     formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
 
     handler = logging.StreamHandler()
@@ -11,3 +12,8 @@ def setup_custom_logger(name):
     logger.setLevel(logging.DEBUG)
     logger.addHandler(handler)
     return logger
+
+
+malamute_logger = setup_custom_logger('malamute')
+
+
